6113d1ceb2e87e85e968819944455953ff3d0c72
BezborodovPavel <bezborodovp@gmail.com>
2022-07-20 13:57:31 +0500

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Павел on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
